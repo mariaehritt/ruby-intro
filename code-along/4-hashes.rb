81bc2337cd44a41c0b4f0ee3005dcc2f862c6990
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-01-27 02:54:33 +0000

    Code along 4

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,30 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+    name: "Ben",
+    location: {
+        city: "Chicago",
+        state: "IL"
+    },
+    timeline: [
+        {status: "Teaching ENTR-451!", posted at: "8:51pm"}
+        {status: "Change Shi's diaper", posted_at: "10:00pm"}
+        {status: "Watch Ozark", posted_at: "10:30pm"}
+    ]
+}
+puts profile
+# puts profile[:status]
+# name = profile[:name]
+# puts name
+puts profile[:location][:city]
+puts profile[:location][:state]
 
-# Accessing data from the hash
+# Accessing data from the hash.
+puts "Latest status:"
+puts "__________"
+puts profile[:timeline][2][:status]
 
 # More Complex Hashes
+profile[:profession] = "Software Dev"
+puts profile
