6cd9ea23e44bfaf7d5f2f690fe5b823bb0536822
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-01-27 01:22:00 +0000

    lab 1 breakout output

--- labs/1-data.rb
+++ labs/1-data.rb
@@ -20,3 +20,11 @@
 # "i would like " + 5 + " tacos"
 # To rectify this, you can convert the number into a string
 # "i would like #{5} tacos"
+
+dice_1 = rand(1..6)
+dice_2 = rand(1..6)
+
+# puts dice_1
+# puts dice_2
+
+puts "#{dice_1} #{dice_2}"
