3bb4b574d36056d025e68e7471cdde5185594865
mariaehritt <maria.ehritt@kellogg.northwestern.edu>
2022-01-27 03:23:51 +0000

    Code along 5

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,8 +2,15 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
-things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts"]
+things_that_would_be_better_with_bacon = ["tacos", "cheeseburgers", "donuts", "ice cream", "peanut butter"]
 
 # The Old Way
+# puts "bacon #{things_that_would_be_better_with_bacon[0]}"
+# puts "bacon #{things_that_would_be_better_with_bacon[1]}"
+# puts "bacon #{things_that_would_be_better_with_bacon[2]}"
 
 # The New Way
+for thing in things_that_would_be_better_with_bacon # thing is singular intentionally
+    # do some code
+    puts "bacon #{thing}" 
+end
